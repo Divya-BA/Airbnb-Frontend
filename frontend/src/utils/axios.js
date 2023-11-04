a929b3fa99b6b1d2181ea058c0725c3e7a44261b
Divya-BA <divyaanbalagan28@gmail.com>
2023-11-04 20:29:17 +0530

    add

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL:'https://airbnb-zegy.onrender.com',
+  baseURL:'https://zippy-sorbet-c69db2.netlify.app',
   withCredentials: true,
 });
 
