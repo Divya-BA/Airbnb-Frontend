278330a73b838c27a2a5167cfc89e366ad110ff4
Divya-BA <divyaanbalagan28@gmail.com>
2023-11-04 20:27:27 +0530

    add

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL:'https://airbnb-zegy.onrender.com',
   withCredentials: true,
 });
 
