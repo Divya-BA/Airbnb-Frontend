8d61c99724dba9533864d88381cfab539184de31
Divya-BA <divyaanbalagan28@gmail.com>
2023-11-05 12:44:32 +0530

    add

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
-  // baseURL:'https://airbnb-6lo2.onrender.com',
   withCredentials: true,
 });
 
